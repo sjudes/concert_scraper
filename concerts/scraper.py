c5f9e4fd2d8baeed8da3d5f5aac16a47791cbbf6
Simon Judes <sjudes@fastmail.com>
2024-12-01 20:49:42 +0000

    Improvements to prompt, and test script.

--- concerts/scraper.py
+++ concerts/scraper.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+import re
+import json
 
 from concerts.schemas import concert_schema
 from concerts.html import get_html
@@ -11,13 +13,30 @@ def extract_concert_info(url):
     now = datetime.now()
     # Create the prompt
     prompt = f"""
-    This is the webpage for one or more concerts.  Please analyse the page, and
-    for each concert, extract:
+    You are an expert at extracting information about classical music concerts from 
+    websites of concert venues and festivals.
+
+    Below in <html_content> tags is some html extracted from a concert website.
+    <html_content>
+    {html_content}
+    </html_content>
+
+    Please analyse the page, and extract the following information for each concert:
+
     1. All musical works (composer and title)
     2. All performers and their roles
     3. Concert date, time, and venue
 
+    Output Specification:
+    1. Return the data in json format as a list of concerts.
+    2. Wrap the list with <extracted_concert_json> tags.
+    3. For each individual concert, use the following schema:
+    {concert_schema}
+
     Some further instructions:
+    - If there are multiple concerts please return the information for all of them in the
+      response to this query.  Please don't return just a sample and ask if further 
+      concerts are desired.
     - Please use the correct case for names, i.e. capitalized first letters.
     - Please put dates in the format DD.MM.YYYY
     - Please just give the start time, not the end time, and use 12-hour format.
@@ -25,73 +44,11 @@ def extract_concert_info(url):
     - For the scrape_info date, please use {now.strftime('%d.%m.%Y')} 
     - For the scrape_info time, please use {now.strftime('%-I:%M %p')}
     - For the scrape_info url, please use {url}
-    
-    Return the data as a list of concerts, with the data for each individual
-    concert in JSON format, using the following schema:
-    {concert_schema}
-
-    Webpage content:
-    {html_content}
     """
 
     # Get response from Claude
-    return claude_query(prompt)
+    claude_response = claude_query(prompt)[0].text
+    concert_info = re.findall("<extracted_concert_json>(.+?)</extracted_concert_json>", 
+                      claude_response, re.DOTALL)
+    return [json.loads(c) for c in concert_info]
     
-
-def extract_concerts_improved(url):
-    """Extracts concert information from a given URL."""
-    html_content = get_html(url)
-    now = datetime.now()
-
-    prompt = f"""
-    You are a sophisticated AI assistant tasked with extracting and structuring concert information from webpage content. Your goal is to analyze the provided HTML and create a structured list of concert data.
-
-    Here's the webpage content to analyze:
-
-    <html_content>
-    {html_content}
-    </html_content>
-
-    Now, here's the schema for each concert object you'll need to create:
-
-    <concert_schema>
-    {concert_schema}
-    </concert_schema>
-
-    Please follow these instructions carefully:
-
-    1. Analyze the webpage content and identify individual concerts.
-
-    2. For each concert, extract the following information:
-        a. All musical works (composer and title)
-        b. All performers and their roles
-        c. Concert date, time, and venue
-
-    3. Format the extracted information according to these rules:
-        - Use correct case for names (capitalized first letters)
-        - Put dates in the format DD.MM.YYYY
-        - Give only the start time, using 12-hour format
-        - Capitalize the first letter of the performer role
-        - Use the current date ({now.strftime('%d.%m.%Y')}) for the scrape_info date
-        - Use the current time ({now.strftime('%-I:%M %p')}) for the scrape_info time
-        - Use {url} for the scrape_info url
-
-    4. Structure each concert's data as a JSON object following the provided schema.
-
-    5. Compile all concert objects into a list.
-
-    6. Wrap the list of concerts inside <extracted_concert_list> tags.
-
-    Before providing the final list of concerts, wrap your analysis inside <extraction_process> tags to show your thought process for extracting and structuring the information. This will help ensure accuracy and completeness. In this analysis:
-
-        1. Identify and list all concert-related sections in the HTML content.
-        2. Extract and list all dates, times, venues, performers, and works mentioned.
-        3. Match these elements to create individual concert objects.
-        4. Verify each concert object against the schema.
-
-    It's OK for this section to be quite long.
-
-    Now, please analyze the webpage content and extract the required concert information.
-    """
-    
-    return claude_query(prompt)
--- test.py
+++ test.py
@@ -1,10 +1,13 @@
 from concerts.scraper import extract_concert_info
+from concerts.venues import WigmoreHall
 
 # url = "https://www.verbierfestival.com/en/programme/?"
 # url = "https://www.verbierfestival.com/en/show/vf25-07-17-1100-150e-anniversaire-de-ravel-partie-1/"
 # url = "https://www.barbican.org.uk/whats-on/2025/event/simon-bolivar-symphony-orchestradudamel-mahler-3"
+# url = "https://www.barbican.org.uk/whats-on/2025/event/shostakovich-quartets-intimate-portraits-part-1"
+# url = "https://www.wigmore-hall.org.uk/whats-on/202412021930"
 
-url = "https://www.barbican.org.uk/whats-on/2025/event/shostakovich-quartets-intimate-portraits-part-1"
-concerts = extract_concert_info(url)
-tb = concerts[0]
-print(tb.dict()['text'])
+# TODO: Parallelise this
+listings = WigmoreHall.get_listing_urls()
+concerts = [extract_concert_info(listing) for listing in listings[:3]]
+from IPython import embed; embed()
